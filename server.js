1b28e0340a3be80d60cf5b1a280b992e8763f46e
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-22 13:50:11 +0100

    Added the cors, helmet and moran middleware

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const logger = require('morgan');
 
 const server = express();
 const router = require('./route');
 
 server.use(express.json());
+server.use(cors());
+server.use(helmet());
+server.use(logger('dev'));
 
 server.get('/', (req, res) =>
   res.status(200).json({
