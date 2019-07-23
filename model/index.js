e226277a51bfea1306366b4f8e35b668fc7921c2
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-23 13:07:19 +0100

    helper function to find a user

--- model/index.js
+++ model/index.js
@@ -3,9 +3,17 @@ const db = require('../data/dbConfig');
 const add = async user => {
   const [id] = await db('users').insert(user);
 
-  return db('users').where({ id });
+  return db('users')
+    .where({ id })
+    .first();
 };
+
+const findBy = username =>
+  db('users')
+    .where({ username })
+    .first();
 
 module.exports = {
   add,
+  findBy,
 };
